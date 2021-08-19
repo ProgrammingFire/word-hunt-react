0b1b4f759837f51ad3cd0e134cbc44687ed40daf
ProgrammingFire <nouman.rahman2010@gmail.com>
2021-08-19 14:27:59 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import axios, { AxiosResponse } from "axios";
+import "./App.css";
 
 function App() {
+  const [meanings, setMeanings] = useState<AxiosResponse | any | Object>([]);
+  const [word, setWord] = useState<String | null>("");
+
+  const fetchData: Function = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+      );
+      setMeanings(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const inputEl = useRef<HTMLInputElement | Object | any>({});
+
+  console.log(inputEl.current.value);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Your Word: </h3>
+      <input type="text" ref={inputEl} />
+      {meanings.map((meaning: any | string) => (
+        <>
+          <h3>{meaning.word}</h3>
+          {meaning.meanings.map((wordMeanings: any | Object) => (
+            <ul style={{ listStyle: "none" }}>
+              <h4>{wordMeanings.partOfSpeech}</h4>
+              {wordMeanings.definitions.map((wordDef: any | []) => (
+                <>
+                  <li>{wordDef.definition}</li>
+                </>
+              ))}
+            </ul>
+          ))}
+        </>
+      ))}
     </div>
   );
 }
